eb2ab87d6f27175abf9973653f0e7a195e6f75ee
Qwei <david22294@gmail.com>
2022-04-24 18:05:37 +0800

    use make_unique to new object

--- main.cpp
+++ main.cpp
@@ -9,8 +9,11 @@ int main()
     modelDuck->performFly();
     modelDuck->performQuack();
 
-    modelDuck->setFlyBehavior(unique_ptr<FlyBehavior>(new FlyWithWings()));
-    modelDuck->setQuackBehavior(unique_ptr<QuackBehavior>(new Quack()));
+    // modelDuck->setFlyBehavior(unique_ptr<FlyBehavior>(new FlyWithWings()));
+    // modelDuck->setQuackBehavior(unique_ptr<QuackBehavior>(new Quack()));
+
+    modelDuck->setFlyBehavior(make_unique<FlyWithWings>());
+    modelDuck->setQuackBehavior(make_unique<Quack>());
 
     modelDuck->performFly();
     modelDuck->performQuack();
